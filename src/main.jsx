5055b10a10ab0aaac671156fa4889cb8559989d7
구혁모 <gurah129@gmail.com>
2025-05-09 17:52:24 +0900

    ✨ 라우터 설정 및 global 스타일 추가

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,32 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
+// createBrowserRouter 방식
+import React, { StrictMode } from "react";
+import ReactDOM from "react-dom/client";
+import "../src/styles/global.css";
+import App from "./App.jsx";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
-createRoot(document.getElementById('root')).render(
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+  },
+  // { 컴포넌트 구현한 뒤 페이지 만들어지면 주석 제거
+  //   path: "/login",
+  //   element: <LoginPage />,
+  // },
+  // {
+  //   path: "/attendance",
+  //   element: <AttendancePage />,
+  // },
+  // {
+  //   path: "/calender",
+  //   element: <AttendanceCheckPage />,
+  // },
+]);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>,
-)
+);
