15987bdcda8ef8c3630313583b445ac40587f6cc
구혁모 <gurah129@gmail.com>
2025-05-09 19:58:16 +0900

    🎨 로고, 내정보 경로는 일단 기본으로 이동

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,16 +2,18 @@ import { Link, useLocation } from "react-router-dom";
 import { logo, menu, profile, search } from "../assets";
 import "../styles/header.css";
 
+// 모든 페이지에서 사용하는게 나을지, 베이스 레이아웃을 추가할지?
 export const Header = () => {
   const location = useLocation();
 
+  // 현재 페이지 위치를 알아내어 current 클래스 추가
   const getNavClass = (path) => {
     return location.pathname === path ? "nav-item current" : "nav-item";
   };
 
   return (
     <div className="container">
-      <Link to="/attendance" className="logo">
+      <Link to="/" className="logo">
         <img src={logo} />
       </Link>
       <div className="nav">
@@ -24,7 +26,7 @@ export const Header = () => {
         <Link to="/attendanceCheck" className={getNavClass("/attendancePage")}>
           출격 확인
         </Link>
-        <Link to="/profile" className={getNavClass("/")}>
+        <Link to="/" className={getNavClass("/")}>
           내 정보
         </Link>
       </div>
