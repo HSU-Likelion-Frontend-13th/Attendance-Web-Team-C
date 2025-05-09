92f81848d29bb64c73483aa5acdffd7026b6800d
구혁모 <gurah129@gmail.com>
2025-05-09 19:25:26 +0900

    💄 헤더 스타일 구현 및 링크로 연결

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,17 +1,18 @@
+import { Link } from "react-router-dom";
 import { logo, menu, profile, search } from "../assets";
 import "../styles/header.css";
 
 export const Header = () => {
   return (
     <div className="container">
-      <div className="logo">
+      <Link to="/attendance" className="logo">
         <img src={logo} />
-      </div>
+      </Link>
       <div className="navi">
-        <div>로그인 / 회원가입</div>
-        <div>출석하기</div>
-        <div>출격 확인</div>
-        <div>내 정보</div>
+        <Link to="/login">로그인 / 회원가입</Link>
+        <Link to="/attendance">출석하기</Link>
+        <Link to="/attendanceCheck">출격 확인</Link>
+        <Link to="/profile">내 정보</Link>
       </div>
       <div className="util">
         <img src={profile} />
