26d936e4580aff24941008fb6f8dfed2b256a8d4
구혁모 <gurah129@gmail.com>
2025-05-09 19:41:32 +0900

    ✨ 테스트를 위한 페이지 추가

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,7 @@ import ReactDOM from "react-dom/client";
 import "../src/styles/global.css";
 import App from "./App.jsx";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { TestPage } from "./pages/TestPage.jsx";
 
 const router = createBrowserRouter([
   {
@@ -22,11 +23,16 @@ const router = createBrowserRouter([
   //   path: "/calender",
   //   element: <AttendanceCheckPage />,
   // },
+  // 테스트를 위한 페이지 추가
+  { 
+    path: "/test",
+    element: <TestPage />,
+  },
 ]);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <StrictMode>
     <RouterProvider router={router} />
-  </StrictMode>,
+  </StrictMode>
 );
---
+++ src/pages/TestPage.jsx
@@ -0,0 +1,5 @@
+import { Header } from "../components/Header";
+
+export const TestPage = () => {
+  return <Header />;
+};
