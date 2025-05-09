1401345f66affbb0b0a1e88c58676176b4a1806d
구혁모 <gurah129@gmail.com>
2025-05-09 19:36:39 +0900

    ✨ 경로에 따라 Header 링크에 액션 클래스 추가

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,18 +1,32 @@
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 import { logo, menu, profile, search } from "../assets";
 import "../styles/header.css";
 
 export const Header = () => {
+  const location = useLocation();
+
+  const getNavClass = (path) => {
+    return location.pathname === path ? "nav-item current" : "nav-item";
+  };
+
   return (
     <div className="container">
       <Link to="/attendance" className="logo">
         <img src={logo} />
       </Link>
-      <div className="navi">
-        <Link to="/login">로그인 / 회원가입</Link>
-        <Link to="/attendance">출석하기</Link>
-        <Link to="/attendanceCheck">출격 확인</Link>
-        <Link to="/profile">내 정보</Link>
+      <div className="nav">
+        <Link to="/login" className={getNavClass("/login")}>
+          로그인 / 회원가입
+        </Link>
+        <Link to="/attendance" className={getNavClass("/attendance")}>
+          출석하기
+        </Link>
+        <Link to="/attendanceCheck" className={getNavClass("/attendancePage")}>
+          출격 확인
+        </Link>
+        <Link to="/profile" className={getNavClass("/")}>
+          내 정보
+        </Link>
       </div>
       <div className="util">
         <img src={profile} />
